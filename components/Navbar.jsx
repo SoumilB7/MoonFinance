e76b26df4475fe398f7f709b2b1760d1f4797219
SamShah30112004 <harshdeepashah@gmail.com>
2024-03-22 23:19:16 +0530

    Made final changes to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,14 +5,14 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className="flex bg-black justify-between items-center p-2">
+    <div className="flex bg-black justify-between items-center p-2 mx-2">
         <div><Image src={logo} width={175} height={175} alt='logo' className="object-contain"/></div>
-        <div className="space-x-4 text-xl">
-            <Link className="text-blue-500 hover:text-blue-800" href='/'>Home</Link>
-            <Link className="text-blue-500 hover:text-blue-800" href='/about'>About</Link>
-            <Link className="text-blue-500 hover:text-blue-800" href='/services'>Services</Link>
-            {/* <Link className="text-blue-500 hover:text-blue-800">Blog</Link> */}
-            <Link className="text-blue-500 hover:text-blue-800" href='/contactUs'>Contact Us</Link>
+        <div className="space-x-6 text-xl text-white ">
+            <Link className="text-white p-2 hover:border-2 hover:border-white hover:rounded-lg" href='/'>Home</Link>
+            <Link className="text-white p-2 hover:border-2 hover:border-white hover:rounded-lg" href='/about'>About</Link>
+            <Link className="text-white p-2 hover:border-2 hover:border-white hover:rounded-lg" href='/services'>Services</Link>
+            {/* <Link className="text-white p-2 hover:border-2 hover:border-white">Blog</Link> */}
+            <Link className="text-white p-2 hover:border-2 hover:border-white hover:rounded-lg" href='/contactUs'>Contact Us</Link>
         </div>
         <div className="space-x-4">
             <Link href='login'><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button></Link>
