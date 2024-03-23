5b98fa561a9113e75778c515d6b2dd8de4ffc529
SamShah30112004 <harshdeepashah@gmail.com>
2024-03-24 00:47:06 +0530

    Added responsiveness

--- components/Home/faq.jsx
+++ components/Home/faq.jsx
@@ -2,35 +2,35 @@ import React from 'react'
 
 const Faq = () => {
   return (
-    <div className='bg-[#E6E6E6] flex flex-row items-center justify-center p-10'>
-      <div className='flex flex-col justify-center items-beggining w-1/2 m-10 p-10'>
-        <p className='text-4xl text-black font-bold'>-FAQ&apos;S</p>
-        <h1 className='text-8xl text-black font-extrabold'>Questions ?? Look Here.</h1>
+    <div className='bg-[#E6E6E6] flex flex-col md:flex-row items-center justify-center p-2 md:p-10'>
+      <div className='flex flex-col justify-center items-beggining w-full md:w-1/2 p-5'>
+        <p className='text-2xl md:text-4xl text-black font-bold'>-FAQ&apos;S</p>
+        <h1 className='text-4xl md:text-8xl text-black font-extrabold'>Questions ?? Look Here.</h1>
       </div>
-      <div className='flex flex-col items-start justify-center w-1/2 p-20 space-y-5'>
-        <details className='text-black text-xl font-semibold w-full'>
+      <div className='flex flex-col items-start justify-center w-full md:w-1/2 p-5 md:p-20 space-y-5'>
+        <details className='text-black text-lg md:text-xl font-semibold w-full'>
           <summary className='list-none'>How does MoonFinance Work? <span className='float-right'>&gt;</span></summary>
-          <p className='text-black text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
+          <p className='text-black text-sm md:text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
         </details>
         <div className='border-t border-black w-full'/>
-        <details className='text-black text-xl font-semibold w-full'>
+        <details className='text-black text-lg md:text-xl font-semibold w-full'>
           <summary className='list-none'>Why should I use MoonFinance? <span className='float-right'>&gt;</span></summary>
-          <p className='text-black text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
+          <p className='text-black text-sm md:text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
         </details>
         <div className='border-t border-black w-full'/>
-        <details className='text-black text-xl font-semibold w-full'>
+        <details className='text-black text-lg md:text-xl font-semibold w-full'>
           <summary className='list-none'>How is my money invested? <span className='float-right'>&gt;</span></summary>
-          <p className='text-black text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
+          <p className='text-black text-sm md:text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
         </details>
         <div className='border-t border-black w-full'/>
-        <details className='text-black text-xl font-semibold w-full'>
+        <details className='text-black text-lg md:text-xl font-semibold w-full'>
           <summary className='list-none'>Where is my money invested? <span className='float-right'>&gt;</span></summary>
-          <p className='text-black text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
+          <p className='text-black text-sm md:text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
         </details>
         <div className='border-t border-black w-full'/>
-        <details className='text-black text-xl font-semibold w-full'>
+        <details className='text-black text-lg md:text-xl font-semibold w-full'>
           <summary className='list-none'>Is my money safe with MoonFinance? <span className='float-right'>&gt;</span></summary>
-          <p className='text-black text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
+          <p className='text-black text-sm md:text-lg'>MoonFinance works by using advanced algorithms and AI to tailor personalized investment portfolios.</p>
         </details>
         <div className='border-t border-black w-full'/>
       </div>
--- components/Home/recognizition.jsx
+++ components/Home/recognizition.jsx
@@ -7,11 +7,11 @@ import vit from '../../public/home/vit.png';
 const Recognizition = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-[#1B1B1B]">
-      <h1 className="text-6xl m-20 font-bold">We Are Recognized By</h1>
-      <div className="flex flex-row items-center justify-center w-full h-[30vh] space-x-20 pb-20">
-        <Image src={microsoft} alt="Recognizition 1" width={350} height={350} className='mt-7'/>
-        <Image src={ecount} alt="Recognizition 1" width={200} height={200} />
-        <Image src={vit} alt="Recognizition 1" width={350} height={350} />
+      <h1 className="text-4xl md:text-6xl m-10 md:m-20 font-bold">We Are Recognized By</h1>
+      <div className="flex flex-col md:flex-row items-center justify-center w-full md:h-[30vh] h-[75vh] md:space-x-20 pb-20">
+        <Image src={microsoft} alt="Recognizition 1" width={200} height={200} className='mt-7 md:mt-0'/>
+        <Image src={ecount} alt="Recognizition 2" width={150} height={150} className='mt-7 md:mt-0'/>
+        <Image src={vit} alt="Recognizition 3" width={200} height={200} className='mt-7 md:mt-0'/>
       </div>
     </div>
   );
