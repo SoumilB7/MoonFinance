342b3d2de672f4126d02b9a5a711e9fb5b35cf35
SamShah30112004 <harshdeepashah@gmail.com>
2024-03-24 22:07:51 +0530

    Bettered responsiveness

--- components/Home/recognizition.jsx
+++ components/Home/recognizition.jsx
@@ -7,11 +7,11 @@ import vit from '../../public/home/vit.png';
 const Recognizition = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-[#1B1B1B]">
-      <h1 className="text-4xl md:text-6xl m-10 md:m-20 font-bold">We Are Recognized By</h1>
-      <div className="flex flex-col md:flex-row items-center justify-center w-full md:h-[30vh] h-[75vh] md:space-x-20 pb-20">
-        <Image src={microsoft} alt="Recognizition 1" width={200} height={200} className='mt-7 md:mt-0'/>
-        <Image src={ecount} alt="Recognizition 2" width={150} height={150} className='mt-7 md:mt-0'/>
-        <Image src={vit} alt="Recognizition 3" width={200} height={200} className='mt-7 md:mt-0'/>
+      <h1 className="text-4xl lg:text-6xl md:text-5xl m-10 lg:m-20 font-bold">We Are Recognized By</h1>
+      <div className="flex flex-col lg:flex-row items-center justify-center w-full lg:h-[30vh] h-[90vh] md:h-[70vh] lg:space-x-20 pb-20">
+        <Image src={microsoft} alt="Recognizition 1" width={300} height={300} className='mt-7 lg:mt-0'/>
+        <Image src={ecount} alt="Recognizition 2" width={250} height={250} className='mt-7 lg:mt-0'/>
+        <Image src={vit} alt="Recognizition 3" width={300} height={300} className='mt-7 lg:mt-0'/>
       </div>
     </div>
   );
