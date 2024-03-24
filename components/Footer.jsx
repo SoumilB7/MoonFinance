e80712e4b5df003809d2ec1e734391d0f1d8e8ee
Hrushiingole <hrishikeshingole532@gmail.com>
2024-03-24 23:07:19 +0530

    about

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+export default function Footer() {
+  return (
+    <>
+
+    <hr />
+    <div className='footer_container'>
+        <div className='footer_left'>
+
+        </div>
+        <div className='footer_right'>
+
+        </div>
+    </div>
+    </>
+  )
+}
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
+import About from '@/components/About/About'
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const about = () => {
-  return (
-    <div>
-      About
-    </div>
+  return (<>
+    <About/>
+    </>
   )
 }
 
