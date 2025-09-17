bc98e731c878b4cf4444f43edae2a4cd3995cdf4
SoumilB7 <kiddu7704@gmail.com>
2025-09-17 13:19:22 +0000

    initiation - 3

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
 // import { clerkMiddleware } from "@clerk/nextjs/server";
 
 // export default clerkMiddleware();
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(_request: NextRequest) {
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: [
